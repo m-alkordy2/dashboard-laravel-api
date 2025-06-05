35c1a604bb7a3e7a3a688ccf0e87671c2bdbe8ec
M-Alkordy <moda-233@hotmail.com>
2025-06-05 19:21:42 +0300

    edit image url to register response

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -162,12 +162,20 @@ class AuthController extends Controller
 
         $token = $user->createToken($validatedData['email'])->plainTextToken;
 
+        $userInfo = [
+            'first_name' => $user->first_name,
+            'last_name' => $user->last_name,
+            'user_name' => $user->user_name,
+            'email' => $user->email,
+            'profile_image_url' => $user->image_url,
+        ];
+
         $response = [
             'status' => 'success',
             'message' => 'User is created successfully.',
             'data' => [
                 'token' => $token,
-                'user' => $user,
+                'user' => $userInfo,
             ],
         ];
 
